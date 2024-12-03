0a18649d54794c390f1694ed17e325836bea795f
Brini <brini.mohamedayechi@gmail.com>
2024-12-03 02:13:44 +0100

    Model Trainer HyperParameters

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 import pickle
+from sklearn.model_selection import KFold,GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,15 +20,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
-
+        cv_strategy = KFold(n_splits=5, shuffle=True, random_state=42)
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+  
+            gs.fit(X_train,y_train)
             #Training Model
-            model.fit(X_train, y_train)  
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train) 
 
             y_test_pred = model.predict(X_test)
 
